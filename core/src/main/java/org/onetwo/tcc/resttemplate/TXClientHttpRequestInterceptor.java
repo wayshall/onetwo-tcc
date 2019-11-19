fb40225a02195285ba40f01ae7fc8cd9d9187429
wayshall <weishao.zeng@gmail.com>
2019-11-19 11:33:29 +0800

    fix client interceptor

--- core/src/main/java/org/onetwo/tcc/resttemplate/TXClientHttpRequestInterceptor.java
+++ core/src/main/java/org/onetwo/tcc/resttemplate/TXClientHttpRequestInterceptor.java
@@ -3,6 +3,7 @@ package org.onetwo.tcc.resttemplate;
 import java.io.IOException;
 
 import org.onetwo.tcc.core.internal.DefaultRemoteTXContextLookupService;
+import org.onetwo.tcc.core.internal.TransactionResourceHolder;
 import org.onetwo.tcc.core.util.TCCInvokeContext;
 import org.springframework.http.HttpRequest;
 import org.springframework.http.client.ClientHttpRequestExecution;
@@ -20,10 +21,13 @@ public class TXClientHttpRequestInterceptor implements ClientHttpRequestIntercep
 
 	@Override
 	public ClientHttpResponse intercept(HttpRequest request, byte[] body, ClientHttpRequestExecution execution) throws IOException {
-		request.getHeaders().set(DefaultRemoteTXContextLookupService.HEADER_GTXID, TCCInvokeContext.get().getGtxId());
-		request.getHeaders().set(DefaultRemoteTXContextLookupService.HEADER_PTXID, TCCInvokeContext.get().getCurrentTxid());
-		if(log.isDebugEnabled()){
-			log.debug("set tcc header spring resttemplate request, gid:{}, pid:{}", TCCInvokeContext.get().getGtxId(), TCCInvokeContext.get().getCurrentTxid());
+		TransactionResourceHolder txHolder = TCCInvokeContext.get();
+		if (txHolder!=null) {
+			request.getHeaders().set(DefaultRemoteTXContextLookupService.HEADER_GTXID, txHolder.getGtxId());
+			request.getHeaders().set(DefaultRemoteTXContextLookupService.HEADER_PTXID, txHolder.getCurrentTxid());
+			if(log.isDebugEnabled()){
+				log.debug("set tcc header spring resttemplate request, gid:{}, pid:{}", txHolder.getGtxId(), txHolder.getCurrentTxid());
+			}
 		}
 		return execution.execute(request, body);
 	}
