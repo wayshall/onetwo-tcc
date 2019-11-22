ce09644993864d1d13ee304cc72ad2cbd62faef5
wayshall <weishao.zeng@gmail.com>
2019-11-22 12:12:41 +0800

    fix tcc producer

--- core/src/main/java/org/onetwo/tcc/core/internal/TCCProducerService.java
+++ core/src/main/java/org/onetwo/tcc/core/internal/TCCProducerService.java
@@ -2,16 +2,21 @@ package org.onetwo.tcc.core.internal;
 
 import org.onetwo.ext.ons.producer.ONSProducerServiceImpl;
 import org.onetwo.tcc.core.TCCProperties;
+import org.springframework.beans.factory.annotation.Value;
 
 /**
  * @author weishao zeng
  * <br/>
  */
 public class TCCProducerService extends ONSProducerServiceImpl {
+	
+	@Value(TCCProperties.PRODUER_ID)
+	private String producerId;
 
 	@Override
 	public void afterPropertiesSet() throws Exception {
-		this.setProducerId(TCCProperties.PRODUER_ID);
+		this.setProducerId(producerId);
+		super.afterPropertiesSet();
 	}
 
 }
